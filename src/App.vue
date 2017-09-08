c35288f9916dbac041a3c7cdeb92861ac14f12a0
markphz <puhaozhou@163.com>
2017-09-08 11:19:44 +0800

    create tableappend compment

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <router-view 
+    :data="gridData"
+    :columns="gridColumns"></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      gridColumns:[
+        { ColumnName: "Vehicleid", Name: "VehicelID" },
+        { ColumnName: "Brand", Name: "品牌" },
+        { ColumnName: "Vehicle", Name: "车系" },
+        { ColumnName: "minPrice", Name: "最低价(万元)" },
+        { ColumnName: "avgPrice", Name: "均价(万元)" },
+        { ColumnName: "maxPrice", Name: "最高价(万元)" },
+        { ColumnName: "VehicleBodyType", Name: "车身类别" }
+        ],
+        gridData:[
+        {VehicleBodyType:"adhufbwv", maxPrice: 32, avgPrice: 25, minPrice : 20, Vehicle : "abc", Brand : "audi", Vehicleid: "text1"},
+        {VehicleBodyType:"efhwufue", maxPrice: 42, avgPrice: 35, minPrice : 30, Vehicle : "dfg", Brand : "bmw", Vehicleid: "text2"},
+        {VehicleBodyType:"reuhnvue", maxPrice: 52, avgPrice: 45, minPrice : 40, Vehicle : "reg", Brand : "benz", Vehicleid: "text3"}
+        ]
+    }
+  }
 }
 </script>
 
---
+++ src/components/TableAppend.vue
@@ -0,0 +1,39 @@
+<template>
+  <table class="table table-bordered">
+      <thead>
+        <tr>
+            <th v-for="column in columns" :id="column.ColumnName">
+                  {{column.Name}}
+            </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in data">
+          <td v-for="key in keys">
+            {{row[key]}}
+          </td>
+        </tr>
+       </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TableAppend',
+  props:{
+        data:Array,
+        columns:Array
+    },
+    computed: {
+        keys:function(){
+            var result = [];
+            this.columns.map(
+                function(i){
+                    result.push(i.ColumnName);
+                }
+            );
+            return result;
+        }
+    }
+}
+</script>
